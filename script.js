237959b74dc11030692a1d0c659ca10a7d5ba176
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 12:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code her
 	let ans = '#';
+
+	if(shortHex[1] === shortHex[1].toUpperCase()){
+		shortHex = shortHex.toUpperCase();
+	}
+	
 	for(let i=1; i<shortHex.length; i++){
 		ans += shortHex[i];
 		ans += shortHex[i];
@@ -10,4 +15,4 @@ const extendHex = (shortHex) => {
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex));   

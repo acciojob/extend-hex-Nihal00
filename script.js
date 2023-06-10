882c564ea8fb806e77b07581ae216c1607d614b9
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 12:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // write your code her
+	let ans = '#';
+	for(let i=1; i<shortHex.length; i++){
+		ans += shortHex[i];
+		ans += shortHex[i];
+	}
+	return ans;
 };
 
 // Do not change the code below.
